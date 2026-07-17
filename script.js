54db49c6db28948cdaf61005abb912814ad885ae
sridevi4141-svg <sridevi4141@gmail.com>
2026-07-17 16:50:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function ownerLogin(){
+import { db } from "./firebase-config.js";
 
-window.location.href="owner-register.html";
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+async function ownerLogin() {
+
+    const snapshot = await getDocs(collection(db, "owners"));
+
+    if (snapshot.empty) {
+
+        // First Time
+        window.location.href = "owner-register.html";
+
+    } else {
+
+        // Already Account Created
+        window.location.href = "owner-login.html";
+
+    }
 
 }
 
-function staffLogin(){
-
-window.location.href="staff-login.html";
-
-}
+window.ownerLogin = ownerLogin;
